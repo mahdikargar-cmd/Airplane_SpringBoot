f6874fcdd1a1e6bb7a0a1e9699946c18b485f2ea
mahdi <www.mahdikargar@gmail.com>
2025-02-26 11:52:22 +0330

    add frontend folder

---
+++ frontend/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // اطمینان حاصل کنید که فعال است
+  },
+};
+
+export default nextConfig;
